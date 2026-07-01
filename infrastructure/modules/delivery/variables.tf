8a8e43f152a9aedece7c69677717ac2e64613a0e
mingsen hua <82328818+Nex-opensourcehorray@users.noreply.github.com>
2026-07-01 14:59:37 +0800

    feat(terraform): define delivery module inputs

---
+++ infrastructure/modules/delivery/variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "processed_bucket_name" {
+  type = string
+}
+
+variable "processed_bucket_arn" {
+  type = string
+}
+
+variable "processed_bucket_domain_name" {
+  type = string
+}
+
+variable "cloudfront_price_class" {
+  type = string
+}
